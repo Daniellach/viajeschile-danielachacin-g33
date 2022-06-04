febe5cd8dc99d5d5d4d21dce0d77a33274061cf4
Nani <dchacin694@gmail.com>
2022-06-03 23:40:34 -0400

    cards modificadas y tooltip agregado

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+
+$(document).scroll(function () {
+  const y = $("html").scrollTop();
+  y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
+});
